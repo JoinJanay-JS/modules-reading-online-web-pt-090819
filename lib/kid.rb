4e34650f46fbe7e205c7ac2cfea1323db92d2ab7
Janay Bailey <tjanay87@gmail.com>
2019-10-21 17:35:06 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,12 +4,11 @@ require_relative './fancy_dance.rb'
 
 class Kid
   include Dance
-
- 
+  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
   attr_accessor :name
- 
   def initialize(name)
     @name = name
   end
-  
 end
